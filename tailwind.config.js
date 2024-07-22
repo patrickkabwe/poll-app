faf4bba9c57c9471dd8b251fa5f8e0e7a88ded84
Patrick Kabwe <patrickckabwe@gmail.com>
2024-07-22 10:07:47 +0200

    chore: setup tailwind and templ

---
+++ tailwind.config.js
@@ -0,0 +1,8 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./views/**/*.templ", "./views/**/*.go", "./views/**/*.html"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
